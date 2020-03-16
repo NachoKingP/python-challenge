82cfabac65b897b8957a9e7276eeb21d6927c21d
Brian Remite^c <brian.remite@gmail.com^C>
2020-03-16 11:35:25 -0400

    Got code running and calculating, but not getting correct results on Average Change and Greatest Decrease in Profits. Maybe negative numbers not being handled properly?

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,30 @@
 import os
 import csv
 
-# Path to collect data from the Resources folder
-data_csv = os.path.join("Resources", "budget_data.csv")
+#-------------------------------------------
+# Declarations
+#-------------------------------------------
 
 #Declare variables necessary for calculation
+TotMonths = 0
+TotPL = 0.00
+MaxPL = 0.00
+MaxMonth = ""
+MinPL = 0.00
+MinMonth = ""
+PLChange = 0.00
+LastMonthPL = 0.00
+PLAvg = 0.00
+
+#List declaration to track average P/L changes
+PLChanges = []
+
+#-------------------------------------------
+# Code to process file
+#-------------------------------------------
+
+#Path to collect data from the Resources folder
+data_csv = os.path.join("Resources", "budget_data.csv")
 
 #read in file
 with open(data_csv, 'r') as csvfile:
@@ -14,5 +34,51 @@ with open(data_csv, 'r') as csvfile:
     
     #skip header row
     header = next(csvreader)
+    
+    #Parse the data in the file
+    for row in csvreader:
+        
+        #Update totals
+        TotMonths += 1
+        TotPL += int(row[1])
+        
+        #Subtract previous PL total from current to get change
+        PLChange = int(row[1]) - LastMonthPL
+        
+        #Update LastMonthPL value for next calculation
+        LastMonthPL = int(row[1])
+        
+        #Add current P/L changes to running list of total P/L changes
+        PLChanges.append(int(row[1]))
+        
+        #Check to see if current revenue change is greatest
+        if (PLChange > MaxPL):
+            MaxPL = PLChange
+            MaxMonth = row[0]
+        
+        #Check to see if current revenue change is lowest
+        if (PLChange < MaxPL):
+            MinPL = PLChange
+            MinMonth = row[0]
+    
+    #Calculate the average P/L change
+    PLAvg = sum(PLChanges) / len(PLChanges)
+
+#-------------------------------------------
+# Output results to screen
+#-------------------------------------------
+
+print("\n \n \n")
+print("```text")
+print("Financial Analysis")
+print("----------------------------")
+print("Total Months: " + str(TotMonths))
+print("Total: $" + str(TotPL))
+print("Average Change: $" + str(PLAvg))
+print("Greatest Increase in Profits: $" + str(MaxPL) + " in " + str(MaxMonth))
+print("Greatest Decrease in Profits: $" + str(MinPL) + " in " + str(MinMonth))
+print("```")
+    
+    
     
     
